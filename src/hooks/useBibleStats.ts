2a34c6d1e52fe6ad8f4ce4067054dbdf52fc4fa3
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-01-30 17:18:00 -0500

    Added old testament stats

--- src/hooks/useBibleStats.ts
+++ src/hooks/useBibleStats.ts
@@ -1,5 +1,200 @@
 const data = [
   {
+    name: "Genesis",
+    chapters: 50,
+    verses: 1533,
+  },
+  {
+    name: "Exodus",
+    chapters: 40,
+    verses: 1213,
+  },
+  {
+    name: "Leviticus",
+    chapters: 27,
+    verses: 859,
+  },
+  {
+    name: "Numbers",
+    chapters: 36,
+    verses: 1288,
+  },
+  {
+    name: "Deuteronomy",
+    chapters: 34,
+    verses: 959,
+  },
+  {
+    name: "Joshua",
+    chapters: 24,
+    verses: 658,
+  },
+  {
+    name: "Judges",
+    chapters: 21,
+    verses: 618,
+  },
+  {
+    name: "Ruth",
+    chapters: 4,
+    verses: 85,
+  },
+  {
+    name: "1 Samuel",
+    chapters: 31,
+    verses: 810,
+  },
+  {
+    name: "2 Samuel",
+    chapters: 24,
+    verses: 695,
+  },
+  {
+    name: "1 Kings",
+    chapters: 22,
+    verses: 816,
+  },
+  {
+    name: "2 Kings",
+    chapters: 25,
+    verses: 719,
+  },
+  {
+    name: "1 Chronicles",
+    chapters: 29,
+    verses: 942,
+  },
+  {
+    name: "2 Chronicles",
+    chapters: 36,
+    verses: 822,
+  },
+  {
+    name: "Ezra",
+    chapters: 10,
+    verses: 280,
+  },
+  {
+    name: "Nehemiah",
+    chapters: 13,
+    verses: 406,
+  },
+  {
+    name: "Esther",
+    chapters: 10,
+    verses: 167,
+  },
+  {
+    name: "Job",
+    chapters: 42,
+    verses: 1070,
+  },
+  {
+    name: "Psalms",
+    chapters: 150,
+    verses: 2461,
+  },
+  {
+    name: "Proverbs",
+    chapters: 31,
+    verses: 915,
+  },
+  {
+    name: "Ecclesiastes",
+    chapters: 12,
+    verses: 222,
+  },
+  {
+    name: "Song of Solomon",
+    chapters: 8,
+    verses: 117,
+  },
+  {
+    name: "Isaiah",
+    chapters: 66,
+    verses: 1292,
+  },
+  {
+    name: "Jeremiah",
+    chapters: 52,
+    verses: 1364,
+  },
+  {
+    name: "Lamentations",
+    chapters: 5,
+    verses: 154,
+  },
+  {
+    name: "Ezekiel",
+    chapters: 48,
+    verses: 1273,
+  },
+  {
+    name: "Daniel",
+    chapters: 12,
+    verses: 357,
+  },
+  {
+    name: "Hosea",
+    chapters: 14,
+    verses: 197,
+  },
+  {
+    name: "Joel",
+    chapters: 3,
+    verses: 73,
+  },
+  {
+    name: "Amos",
+    chapters: 9,
+    verses: 146,
+  },
+  {
+    name: "Obadiah",
+    chapters: 1,
+    verses: 21,
+  },
+  {
+    name: "Jonah",
+    chapters: 4,
+    verses: 48,
+  },
+  {
+    name: "Micah",
+    chapters: 7,
+    verses: 105,
+  },
+  {
+    name: "Nahum",
+    chapters: 3,
+    verses: 47,
+  },
+  {
+    name: "Habakkuk",
+    chapters: 3,
+    verses: 56,
+  },
+  {
+    name: "Zephaniah",
+    chapters: 3,
+    verses: 53,
+  },
+  {
+    name: "Haggai",
+    chapters: 2,
+    verses: 38,
+  },
+  {
+    name: "Zechariah",
+    chapters: 14,
+    verses: 211,
+  },
+  {
+    name: "Malachi",
+    chapters: 4,
+    verses: 55,
+  },
+  {
     name: "Matthew",
     chapters: 28,
     verses: 1071,
